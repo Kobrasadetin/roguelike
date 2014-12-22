85b6832aec368582354da0021d360b3079c79ad4
Kobrasadetin <mikko.karjanmaa@gmail.com>
2014-12-22 22:33:43 +0200

    Pit-testi, dokumentaatiota.

---
+++ Roguelike/src/test/java/com/majesticbit/roguelike/dungeonbuilder/DungeonBuilderTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.majesticbit.roguelike.dungeonbuilder;
+
+import com.majesticbit.roguelike.domain.BasicDungeon;
+import com.majesticbit.roguelike.domain.Dungeon;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Master
+ */
+public class DungeonBuilderTest {
+
+    private DungeonBuilder instance;
+
+    public DungeonBuilderTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        instance = new DungeonBuilder();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of toDungeon method, of class DungeonBuilder.
+     */
+
+}
